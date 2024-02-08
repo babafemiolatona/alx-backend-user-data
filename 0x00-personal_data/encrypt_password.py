8ee55b5b0634ce5c3c1141d3adb44f74a12ce29d
babafemiolatona <femiolatona@gmail.com>
2024-02-08 21:58:47 +0100

    Encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
-"""Encrypt password"""
+"""
+Defines a hash_password function to return a hashed password
+"""
 import bcrypt
+from bcrypt import hashpw
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """
-    Returns a salted, hashed password
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
     """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
