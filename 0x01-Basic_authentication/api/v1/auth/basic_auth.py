4c368314f9ea67f8b49ddd0e33c63373397bd2c2
babafemiolatona <femiolatona@gmail.com>
2024-02-13 20:42:38 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """BasicAuth Module"""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,3 +14,16 @@ class BasicAuth(Auth):
                 not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode('utf-8')).decode('utf-8')
+        except Exception:
+            return None
