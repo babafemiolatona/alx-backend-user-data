1d73681993c1ba58e1059f4e51c3e9124e5a7cec
babafemiolatona <femiolatona@gmail.com>
2024-02-13 19:25:04 +0100

    Request Validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+AUTH_TYPE = getenv('AUTH_TYPE')
 
 if os.getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
@@ -40,6 +41,22 @@ def forbidden(error) -> str:
     Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> None:
+    """
+    Before request handler
+    """
+    if auth is None:
+        return
+    if auth.require_auth(request.path, ['/api/v1/status/',
+                                        '/api/v1/unauthorized/',
+                                        '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
