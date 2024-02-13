da08d8ba132812ccfa41ef997a6a73e125add430
babafemiolatona <femiolatona@gmail.com>
2024-02-13 22:10:48 +0100

    Basic - Overload current_user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,3 +50,11 @@ class BasicAuth(Auth):
             return User().search({'email': user_email})
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        auth_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(auth_header)
+        user_email, user_pwd = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user_email, user_pwd)
