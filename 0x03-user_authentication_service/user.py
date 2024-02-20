f12180bf862247917d23209c4c38618d7b41ab79
babafemiolatona <femiolatona@gmail.com>
2024-02-21 00:41:54 +0100

    Added documentation

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,15 +2,17 @@
 """User Module"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Sequence
 
 Base = declarative_base()
 
 
 class User(Base):
+    """
+    User model
+    """
     __tablename__ = 'users'
 
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
