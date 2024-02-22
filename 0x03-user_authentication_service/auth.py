9be1d5be1e366cb94ab617e6924dc186dbcc30cc
babafemiolatona <femiolatona@gmail.com>
2024-02-22 21:01:13 +0100

    Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,7 +11,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """Generate UUID"""
     return str(uuid.uuid4())
 
