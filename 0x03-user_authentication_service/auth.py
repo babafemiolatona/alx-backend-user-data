a4a89617322867e19642b17bb9032c4e4372a00c
babafemiolatona <femiolatona@gmail.com>
2024-02-22 22:17:09 +0100

    Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,4 +88,4 @@ class Auth:
 
         hashed_password = _hash_password(password)
         self._db.update_user(user.id,
-                             hashed_password=hashed_password,reset_token=None)
+                             hashed_password=hashed_password, reset_token=None)
